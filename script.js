0436c542e9f9c83c015288d4dbe9043e0bf00f7f
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-10 20:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let student = {
   name: "amit",
   age: 23,
- 
-};
-
-Object.prototype.getKeys=function () {
-      let arr = [];
+  getKeys:function () {
+  	let arr = [];
       for (let key in this) {
         arr.push(key);
       }
       return arr;
-    },
+  }
+};
 
 	
