13d15adcb012525f5178aaef50d328a0397154d1
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-10 20:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let myObj = {
   name: "amit",
   age: 23,
-  getKeys:function () {
+  
+};
+
+myObj.getKeys=function () {
   	let arr = [];
       for (let key in this) {
         arr.push(key);
       }
       return arr;
   }
-};
-
 	
