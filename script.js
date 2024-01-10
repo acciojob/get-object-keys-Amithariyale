39b6862c0f51dd2412693f40306e4add4a852a65
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-10 20:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-let myObj = {
-  name: "amit",
-  age: 23,
+// let myObj = {
+//   name: "amit",
+//   age: 23,
   
+// };
+
+// myObj.getKeys=function () {
+//   	let arr = [];
+//       for (let key in this) {
+//         arr.push(key);
+//       }
+//       return arr;
+//   }
+const student = {
+    name: "John Doe"
 };
 
-myObj.getKeys=function () {
-  	let arr = [];
-      for (let key in this) {
-        arr.push(key);
-      }
-      return arr;
-  }
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+    // Use Object.keys to get an array of all keys in the object
+    return Object.keys(this);
+};
 	
